4fb89a70474b20fb1098232a1f0fb4d37dcdda79
Kamil0108 <kamil.podobinski@tutamail.com>
2026-01-18 23:07:11 +0100

    Fix robots.txt: rozbudowa regul i usuniecie duplikatu
    - Dodano Disallow: /api/, /_next/, /private/
    
    - Dodano host: https://syntance.com
    
    - Usunieto zbedny public/robots.txt (konflikt z app/robots.ts)

--- app/robots.ts
+++ app/robots.ts
@@ -2,8 +2,15 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/_next/", "/private/"],
+      },
+    ],
     sitemap: "https://syntance.com/sitemap.xml",
+    host: "https://syntance.com",
   };
 }
 
